c4563b52c4278fd9ad8a8a4440a8f9b579e2ac20
Ivo Mirra <ivo.mirra@blip.pt>
2016-02-11 11:26:08 +0000

    Update routing

--- js/app.js
+++ js/app.js
@@ -3,67 +3,10 @@ angular.module('project', ['ngRoute']);
 angular.module('project').config(function($routeProvider){
 
 	$routeProvider
-	.when('/', {
-		templateUrl: 'templates/home.html'
+	.when('/slide1', {
+		templateUrl: 'templates/slide1.html'
 	})
-	.when('/who-we-are', {
-        templateUrl: 'templates/who.html'
-    })
-    .when('/products', {
-        templateUrl: 'templates/products.html'
-    })
-    .when('/contacts', {
-        templateUrl: 'templates/contacts.html'
-    })
     .otherwise({
-		redirectTo: '/'
+		redirectTo: '/slide1'
 	});
 });
-
-angular.module('project').service('menuService', 
-	function() {
-		this.getMenuItems = function() {
-			return [
-				{
-					id: 'home',
-					name: 'Home',
-					link: '#/'
-				},
-				{
-					id: 'who-we-are',
-					name: 'Quem Somos',
-					link: '#/who-we-are'
-				},
-				{
-					id: 'products',
-					name: 'Produtos',
-					link: '#/products'
-				},
-				{
-					id: 'contacts',
-					name: 'Contactos',
-					link: '#/contacts'
-				}
-			];
-		}
-	}
-)
-
-angular.module('project').controller('menuController', 
-	function($scope, menuService){
-		$scope.items = menuService.getMenuItems();
-	}
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
